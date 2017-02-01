2ef97d0aae471660fe0a83982a69dede06d13de4
Patty Vader <pattyvaderbr@gmail.com>
2017-02-01 12:20:28 -0200

    Adds initial code to import CVS.

---
+++ work-at-olist/workatolist/management/commands/importcategories.py
@@ -0,0 +1,7 @@
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    help = "My test command"
+
+    def handle(self, *args, **options):
+        self.stdout.write("Import categories!")
