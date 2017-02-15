774e869a1d44f364b22eaa68a983fcb1d86c777f
Patty Vader <pattyvaderbr@gmail.com>
2017-02-15 12:14:14 -0200

    Adds function to capture arguments to import file .csv.

--- work-at-olist/workatolist/management/commands/importcategories.py
+++ work-at-olist/workatolist/management/commands/importcategories.py
@@ -3,5 +3,13 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
     help = "Imports channels's categories"
 
+    def add_arguments(self, parser):
+        parser.add_argument('import_arguments',nargs='*')
+
     def handle(self, *args, **options):
-        self.stdout.write("Import categories!")
+        import_arguments = options['import_arguments']
+
+        channel_name = import_arguments[0]
+        file_name = import_arguments[1]
+
+        #self.stdout.write("Import the channel " + channel_name + " and .csv file " + file_name)
