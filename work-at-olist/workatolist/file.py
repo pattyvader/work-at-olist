602d23c82287c419ea15bfb480f8d7937366a2da
Patty Vader <pattyvaderbr@gmail.com>
2017-03-05 17:21:45 -0300

    Delete the archive 'file.py'

--- work-at-olist/workatolist/file.py
+++
@@ -1,7 +0,0 @@
-def read_file(name_file):
-    try:
-        file_csv = open(name_file, "r")
-
-        return file_csv
-    except IOError as error_import:
-        print("Error: {0}".format(error_import))
