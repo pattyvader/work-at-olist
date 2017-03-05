62624bb7cd1a1034a9dd409f32896d69e471de5f
Patty Vader <pattyvaderbr@gmail.com>
2017-03-05 17:24:42 -0300

    Adds functions inside class 'Channel' and 'Category'.

--- work-at-olist/workatolist/models.py
+++ work-at-olist/workatolist/models.py
@@ -7,6 +7,22 @@ class Channel(models.Model):
     def __str__(self):
         return self.name
 
+    def count_channel(self, name_channel):
+        number_channel = Channel.objects.filter(name = name_channel).count()
+
+        return number_channel
+
+    def get_channel(self, channel_name):
+        channel = Channel.objects.get(name = channel_name)
+
+        return channel
+
+class CategoryManager(models.Manager):
+    def create_category(self, name, parent_id, channel):
+        category = self.create(name = name, parent_id = parent_id, channel = channel)
+
+        return category
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     parent_id = models.IntegerField(blank=True, null=True)
@@ -17,3 +33,5 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('channel', args=[str(self.id)])
+
+    objects = CategoryManager()
