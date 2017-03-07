5bea45c9385e1322010d1c7d5182b3bf54012e13
Patty Vader <pattyvaderbr@gmail.com>
2017-03-06 21:02:28 -0300

    Adds url to api.

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import include, url
+from rest_framework.authtoken.views import obtain_auth_token
+from workatolist.apiworkatolist.urls import router
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/v1.0/', include(router.urls)),
 ]
