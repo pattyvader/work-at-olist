fb24847befffd38489e947f003695830098a5c09
Patty Vader <pattyvaderbr@gmail.com>
2017-03-07 20:04:41 -0300

    Adds new route to categories details.

--- work-at-olist/workatolist/apiworkatolist/urls.py
+++ work-at-olist/workatolist/apiworkatolist/urls.py
@@ -6,3 +6,4 @@ router.register(r'channels', views.ChannelViewSet)
 router.register(r'channels/(?P<id>\d+)/categories', views.ChannelCategoriesDetailViewSet,
     base_name = 'channel-categories-detail')
 router.register(r'categories', views.CategoryViewSet)
+router.register(r'categories/(?P<id>\d+)/details', views.CategoryDetailViewSet, base_name = 'categories-detail')
