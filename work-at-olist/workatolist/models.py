5c8962571ec342561b1238c0f3021c2a2d0472f4
Patty Vader <pattyvaderbr@gmail.com>
2017-03-07 20:07:16 -0300

    Adds the 'parent' property in the class 'Category'.

--- work-at-olist/workatolist/models.py
+++ work-at-olist/workatolist/models.py
@@ -18,15 +18,15 @@ class Channel(models.Model):
         return channel
 
 class CategoryManager(models.Manager):
-    def create_category(self, name, parent_id, channel):
-        category = self.create(name = name, parent_id = parent_id, channel = channel)
+    def create_category(self, name, parent, channel):
+        category = self.create(name = name, parent = parent, channel = channel)
 
         return category
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    parent_id = models.IntegerField(blank=True, null=True)
     channel = models.ForeignKey('Channel', on_delete=models.SET_NULL,null=True,related_name='categories')
+    parent = models.ForeignKey('self',null=True, blank=True,related_name='subcategories')
 
     def __str__(self):
         return self.name
