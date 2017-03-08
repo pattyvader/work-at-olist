91b76ab78ed934db083e16b4e344c3f274a53138
Patty Vader <pattyvaderbr@gmail.com>
2017-03-08 16:17:12 -0300

    Adds driver of the postgresql;

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -67,6 +67,16 @@ WSGI_APPLICATION = 'workatolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
