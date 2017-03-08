be76b0a7c2423def1351a004ec992c9de0be26d4
Patty Vader <pattyvaderbr@gmail.com>
2017-03-07 21:17:40 -0300

    Add category serializers.

--- work-at-olist/workatolist/apiworkatolist/serializers.py
+++ work-at-olist/workatolist/apiworkatolist/serializers.py
@@ -4,7 +4,27 @@ from workatolist.models import Channel, Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('id', 'parent_id', 'name', 'channel', )
+        fields = ('id', 'parent', 'name', 'channel')
+
+class CategoryParentSerializer(serializers.ModelSerializer):
+    parent = CategorySerializer(read_only=True)
+    class Meta:
+        model = Category
+        fields = ('id', 'parent', 'name', 'channel')
+
+
+class CategorySimpleSerializer(serializers.ModelSerializer):
+    parent = CategoryParentSerializer(read_only=True)
+    class Meta:
+        model = Category
+        fields = ('id', 'parent', 'name', 'channel','subcategories')
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    subcategories = CategorySimpleSerializer(many=True, read_only=True)
+    parent = CategorySerializer(read_only=True)
+    class Meta:
+        model = Category
+        fields = ('id', 'parent', 'name', 'channel','subcategories')
 
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
