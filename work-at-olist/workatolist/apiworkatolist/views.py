feb8219dfac18f0a2c8f462f926741c5cab8ca5e
Patty Vader <pattyvaderbr@gmail.com>
2017-03-07 21:20:11 -0300

    Adds the viewset 'CategoryDetailViewSet'

--- work-at-olist/workatolist/apiworkatolist/views.py
+++ work-at-olist/workatolist/apiworkatolist/views.py
@@ -1,6 +1,6 @@
 from rest_framework import authentication, permissions, viewsets
 from workatolist.models import Channel, Category
-from workatolist.apiworkatolist.serializers import ChannelSerializer, CategorySerializer
+from workatolist.apiworkatolist.serializers import ChannelSerializer, CategorySerializer, CategoryDetailSerializer
 
 class ChannelViewSet(viewsets.ModelViewSet):
     queryset = Channel.objects.all()
@@ -9,7 +9,15 @@ class ChannelViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_fields = ('id')
+
+class CategoryDetailViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategoryDetailSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+
+        return Category.objects.filter(parent_id = id)
 
 class ChannelCategoriesDetailViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
