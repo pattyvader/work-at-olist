425dde0e0e91c7b581a89d9823ab941dbf17d470
Patty Vader <pattyvaderbr@gmail.com>
2017-03-08 21:02:11 -0300

    Adds 'name' and 'engine' properties in 'DATABASE'  variable.

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -69,7 +69,8 @@ WSGI_APPLICATION = 'workatolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
     }
 }
 
